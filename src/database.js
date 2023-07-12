5a57e94197fc354cf27b21ca2045d2253038a673
Melina <melii_prak@hotmail.com>
2023-07-12 12:13:16 -0500

    modified:   src/database.js

--- src/database.js
+++ src/database.js
@@ -1,12 +1,14 @@
 const { connect } = require("mongoose");
 const { config } = require("dotenv");
+const mongoose = require("mongoose");
 config();
 
 const MONGODB_URI = "mongodb+srv://meliiprakd2:prakd2@cluster0.ywfuhld.mongodb.net/";
 
 (async() => {
+    mongoose.set('strictQuery', false);
     try {
-        const db = await connect(MONGODB_URI);
+        const db = await connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log("Db connectect to", db.connection.name);
     } catch (error) {
         console.error(error);
