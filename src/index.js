7feccf5b6da011f8048ab0b15f3c29d99f53f232
Melina <melii_prak@hotmail.com>
2023-07-12 15:02:09 -0500

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ require("./database");
 require("./config/passport");
 
 // Settings
-app.set("port", process.debugPort || 3000);
+app.set("port", process.env.PORT || 8080);
 
 // Views
 app.set("views", path.join(__dirname, "views"));
