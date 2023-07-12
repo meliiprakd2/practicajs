843960716d9f84ab0e347b33e38317b16dfa502d
Melina <melii_prak@hotmail.com>
2023-07-12 12:57:54 -0500

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -18,13 +18,13 @@ app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
 
 app.engine(
-  ".hbs",
-  engine({
-    defaultLayout: "main",
-    extname: ".hbs",
-    layoutsDir: path.join(app.get("views"), "layouts"),
-    partialsDir: path.join(app.get("views"), "partials"),
-  })
+    ".hbs",
+    engine({
+        defaultLayout: "main",
+        extname: ".hbs",
+        layoutsDir: path.join(app.get("views"), "layouts"),
+        partialsDir: path.join(app.get("views"), "partials"),
+    })
 );
 app.set("view engine", ".hbs");
 
@@ -32,11 +32,11 @@ app.set("view engine", ".hbs");
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 app.use(
-  session({
-    secret: "mysecretapp",
-    resave: true,
-    saveUninitialized: true,
-  })
+    session({
+        secret: "mysecretapp",
+        resave: true,
+        saveUninitialized: true,
+    })
 );
 app.use(passport.initialize());
 app.use(passport.session());
@@ -44,11 +44,11 @@ app.use(flash());
 
 // Global Variables
 app.use((req, res, next) => {
-  res.locals.succes_msg = req.flash("succes_msg");
-  res.locals.error_msg = req.flash("error_msg");
-  res.locals.error = req.flash("error");
-  res.locals.user = req.user || null;
-  next();
+    res.locals.succes_msg = req.flash("succes_msg");
+    res.locals.error_msg = req.flash("error_msg");
+    res.locals.error = req.flash("error");
+    res.locals.user = req.user || null;
+    next();
 });
 // Routes
 app.use(require("./routes/index.routes"));
@@ -56,9 +56,9 @@ app.use(require("./routes/users.routes"));
 app.use(require("./routes/notes.routes"));
 
 // Static files
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 //Server is listening
 app.listen(app.get("port"), () => {
-  console.log("Server on port", app.get("port"));
+    console.log("Server on port", app.get("port"));
 });
